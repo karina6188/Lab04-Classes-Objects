b9726b95c6320a83f82e73a6e4ced9e810804168
Karina Chen <karina6188@gmail.com>
2019-10-25 23:33:10 -0700

    work on returning winners

--- Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
+++ Lab04_TicTacToe/Lab04_TicTacToe/Program.cs
@@ -18,18 +18,14 @@ namespace Lab04_TicTacToe
             Console.WriteLine("Let's play a tic-tac-toe game!");
             Console.WriteLine("Please enter player 1's name: ");
             string name1 = Console.ReadLine();
-            Console.WriteLine("Please enter player 2's name: ");
+            Console.WriteLine("Please enter player 2's name: \n");
             string name2 = Console.ReadLine();
-            Console.WriteLine("Great! Let's start the game!\n");
             Player player1 = new Player($"{ name1 }(player 1)", "X");
             Player player2 = new Player($"{ name2 }(player 2)", "O");
             Game game = new Game(player2, player1);
-
             game.Play();
             Player winner = (game.Play());
-            Console.WriteLine($"Congratulations!! {winner} won the game!");
         }
-
         // TODO: You are requesting a Winner to be returned, Determine who the winner is output the celebratory message to the correct player. If it's a draw, tell them that there is no winner. 
     }
 }
